02478b65f636cd301c89ed9f69c14862d99c2b90
Adripy <36918572+Adripy@users.noreply.github.com>
2022-12-14 23:34:30 +0100

    Sprint 1 - Fase Verde

---
+++ ClasesTest/Estadistica.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ClasesTest
+{
+    public class Estadistica
+    {
+        public float MediaAritmetica(float[] numeros)
+        {
+            float sum = 0;
+
+            foreach (float numero in numeros) 
+            { 
+                sum += numero;
+            }
+
+            return  (float)Math.Round(sum/numeros.Length,2);
+        }
+
+        public float MediaGeometrica(float[] numeros)
+        {
+            float sum = 1;
+
+            foreach (float numero in numeros)
+            {
+                sum *= numero;
+            }
+            float result = (float)Math.Pow(sum, (float) 1/ numeros.Length);
+
+            return (float)Math.Round(result, 2);
+        }
+
+        public float MediaArmonica(float[] numeros)
+        {
+            float sum = 0;
+
+            for (int i = 0; i < numeros.Length; i++)
+                sum = sum + (float)1 / numeros[i];
+
+            return (float)Math.Round(numeros.Length / sum,2);
+        }
+
+        public float Mediana(float[] numeros)
+        {
+            Array.Sort(numeros);
+
+            if (numeros.Length % 2 != 0)
+                return numeros[((numeros.Length + 1) / 2)-1];
+
+            return (numeros[numeros.Length / 2] + numeros[numeros.Length / 2 - 1]) / 2;
+        }
+
+        public float[] Moda(float[] numeros)
+        {       
+            var dict = numeros.ToLookup(x => x);
+
+            var numeroModas = dict.Max(x => x.Count());
+
+            var modas = dict.Where(x => x.Count() == numeroModas).Select(x => x.Key);
+
+            return modas.ToArray();
+        }
+
+        public float[] DesviacionAbsoluta(float[] numeros)
+        {
+            float[] ordenado = new float[numeros.Length];
+
+            ordenado = (float[])numeros.Clone();
+
+            Array.Sort(ordenado);
+
+            float mediana = 0;
+
+            if (ordenado.Length % 2 != 0)
+            {
+                mediana = ordenado[((ordenado.Length + 1) / 2) - 1];
+            }
+            else 
+            {
+                mediana = (ordenado[ordenado.Length / 2] + ordenado[ordenado.Length / 2 - 1]) / 2;
+            }
+            Console.WriteLine("mediana {0}", mediana);
+
+            for(int i = 0; i < numeros.Length; i++)
+            {
+                ordenado[i] = (float)Math.Round((float)Math.Abs(numeros[i] - mediana),1);
+            }
+
+            return ordenado;
+        }
+
+        public float DesviacionMedia(float[] numeros)
+        {
+            float sum = 0;
+
+            float[] ordenado = new float[numeros.Length];
+
+            ordenado = (float[])numeros.Clone();
+
+            Array.Sort(ordenado);
+
+            float mediana = 0;
+
+            if (ordenado.Length % 2 != 0)
+            {
+                mediana = ordenado[((ordenado.Length + 1) / 2) - 1];
+            }
+            else
+            {
+                mediana = (ordenado[ordenado.Length / 2] + ordenado[ordenado.Length / 2 - 1]) / 2;
+            }
+            Console.WriteLine("mediana {0}", mediana);
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                sum += (float)Math.Round((float)Math.Abs(numeros[i] - mediana), 1);
+            }
+
+            return (float)sum / numeros.Length;
+        }
+    }
+}
--- ClasesTest/EstadisticaTest.cs
+++ ClasesTest/EstadisticaTest.cs
@@ -9,140 +9,185 @@ namespace ClasesTest
         [TestMethod]
         public void MediaAritmeticaTest()
         {
-            int[] enteros = { 1, 2, 3, 4, 5};
+            float[] enteros = { 1, 2, 3, 4, 5 };
             float mediaArEnteros = 3;
             float[] decimales = { 1.5f, 2.3f, 3.2f, 4.5f, 5.1f };
             float mediaArDecimales = 3.32f;
-            char[] caracteres = { 'a', 'b', 'c', 'd', 'e' };
-            int[] prueba = { 50, 22, 53, 4, 15 };
-            float mediaArPrueba = 28.8f;
+            float[] prueba = { 50, 22, 53, 4, 15 };
+            float mediaArPrueba = 28.80f;
 
             Estadistica es = new Estadistica();
 
             Assert.AreEqual(es.MediaAritmetica(enteros), mediaArEnteros);
             Assert.AreEqual(es.MediaAritmetica(decimales), mediaArDecimales);
-            Assert.ThrowsException(es.MediaAritmetica(caracteres));
             Assert.AreEqual(es.MediaAritmetica(prueba), mediaArPrueba);
         }
-    }
 
-    [TestMethod]
-    public void MediaGeometricaTest()
-    {
-        int[] enteros = { 1, 2, 3, 4, 5 };
-        float mediaEnteros = 2.605f;
-        float[] decimales = { 1.5f, 2.3f, 3.2f, 4.5f, 5.1f };
-        float mediaDecimales = 3.025f;
-        char[] caracteres = { 'a', 'b', 'c', 'd', 'e' };
-        int[] prueba = { 50, 22, 53, 4, 15 };
-        float mediaPrueba = 20.359f;
+        [TestMethod]
+        public void MediaGeometricaTest()
+        {
+            float[] enteros = { 1, 2, 3, 4, 5 };
+            float mediaEnteros = 2.61f;
+            float[] decimales = { 1.5f, 2.3f, 3.2f, 4.5f, 5.1f };
+            float mediaDecimales = 3.03f;
+            float[] prueba = { 50, 22, 53, 4, 15 };
+            float mediaPrueba = 20.36f;
 
-        Estadistica es = new Estadistica();
+            Estadistica es = new Estadistica();
 
-        Assert.AreEqual(es.MediaGeometrica(enteros), mediaEnteros);
-        Assert.AreEqual(es.MediaGeometrica(decimales), mediaDecimales);
-        Assert.ThrowsException(es.MediaGeometrica(caracteres));
-        Assert.AreEqual(es.MediaGeometrica(prueba), mediaPrueba);
-    }
+            Assert.AreEqual(es.MediaGeometrica(enteros), mediaEnteros);
+            Assert.AreEqual(es.MediaGeometrica(decimales), mediaDecimales);
+            Assert.AreEqual(es.MediaGeometrica(prueba), mediaPrueba);
+        }
 
-    [TestMethod]
-    public void MediaArmonicaTest()
-    {
-        int[] enteros = { 1, 2, 3, 4, 5 };
-        float mediaEnteros = 2.19f;
-        float[] decimales = { 1.5f, 2.3f, 3.2f, 4.5f, 5.1f };
-        float mediaDecimales = 2.73f;
-        char[] caracteres = { 'a', 'b', 'c', 'd', 'e' };
-        int[] prueba = { 50, 22, 53, 4, 15 };
-        float mediaPrueba = 12.47f;
+        [TestMethod]
+        public void MediaArmonicaTest()
+        {
+            float[] enteros = { 1, 2, 3, 4, 5 };
+            float mediaEnteros = 2.19f;
+            float[] decimales = { 1.5f, 2.3f, 3.2f, 4.5f, 5.1f };
+            float mediaDecimales = 2.73f;
+            float[] prueba = { 50, 22, 53, 4, 15 };
+            float mediaPrueba = 12.47f;
 
-        Estadistica es = new Estadistica();
+            Estadistica es = new Estadistica();
 
-        Assert.AreEqual(es.MediaArmonica(enteros), mediaEnteros);
-        Assert.AreEqual(es.MediaArmonica(decimales), mediaDecimales);
-        Assert.ThrowsException(es.MediaArmonica(caracteres));
-        Assert.AreEqual(es.MediaArmonica(prueba), mediaPrueba);
-    }
+            Assert.AreEqual(es.MediaArmonica(enteros), mediaEnteros);
+            Assert.AreEqual(es.MediaArmonica(decimales), mediaDecimales);
+            Assert.AreEqual(es.MediaArmonica(prueba), mediaPrueba);
+        }
 
-    [TestMethod]
-    public void MedianaTest()
-    {
-        int[] enteros = { 5,4,3,2,1 };
-        float medianaEnteros = 3f;
-        float[] decimales = { 1.5f, 2.3f, 3.2f, 4.5f, 5.1f };
-        float medianaDecimales = 3.2f;
-        char[] caracteres = { 'a', 'b', 'c', 'd', 'e' };
-        int[] pruebaImpar = { 50, 22, 53, 4, 15 };
-        float medianaPruebaImpar = 22f;
-        int[] pruebaPar = { 1, 2, 3, 4};
-        float medianaPruebaPar = 2.5f;
+        [TestMethod]
+        public void MedianaTest()
+        {
+            float[] enteros = { 5, 4, 3, 2, 1 };
+            float medianaEnteros = 3f;
+            float[] decimales = { 1.5f, 2.3f, 3.2f, 4.5f, 5.1f };
+            float medianaDecimales = 3.2f;
+            float[] pruebaImpar = { 50, 22, 53, 4, 15 };
+            float medianaPruebaImpar = 22f;
+            float[] pruebaPar = { 1, 2, 3, 4 };
+            float medianaPruebaPar = 2.5f;
 
-        Estadistica es = new Estadistica();
+            Estadistica es = new Estadistica();
 
-        Assert.AreEqual(es.Mediana(enteros), medianaEnteros);
-        Assert.AreEqual(es.Mediana(decimales), medianaDecimales);
-        Assert.ThrowsException(es.Mediana(caracteres));
-        Assert.AreEqual(es.Mediana(pruebaImpar), medianaPruebaImpar);
-        Assert.AreEqual(es.Mediana(pruebaPar), medianaPruebaPar);
-    }
+            Assert.AreEqual(es.Mediana(enteros), medianaEnteros);
+            Assert.AreEqual(es.Mediana(decimales), medianaDecimales);
+            Assert.AreEqual(es.Mediana(pruebaImpar), medianaPruebaImpar);
+            Assert.AreEqual(es.Mediana(pruebaPar), medianaPruebaPar);
+        }
 
-    [TestMethod]
-    public void ModaTest()
-    {
-        int[] enteros = { 5, 4, 3, 2, 1, 2};
-        float[] solEnteros = { 2f };
-        float[] decimales = { 1.5f, 2.3f, 3.2f, 4.5f, 5.1f, 3.2f};
-        float[] solDecimales = { 3.2f };
-        char[] caracteres = { 'a', 'b', 'c', 'd', 'e' };
-        int[] pruebaModaMult = { 50, 22, 53, 4, 15 ,50 ,53};
-        float[] solPruebaModaMult = { 50, 53 };
-        int[] pruebaSinModa = { 1, 2, 3, 4 };
-        float[] solPruebaSinModa = { };
+        [TestMethod]
+        public void ModaTest()
+        {
+            float[] enteros = { 5, 4, 3, 2, 1, 2 };
+            float[] solEnteros = { 2f };
+            float[] decimales = { 1.5f, 2.3f, 3.2f, 4.5f, 5.1f, 3.2f };
+            float[] solDecimales = { 3.2f };
+            float[] pruebaModaMult = { 50, 22, 53, 4, 15, 50, 53 };
+            float[] solPruebaModaMult = { 50, 53 };
+            float[] pruebaSinModa = { 1, 2, 3, 4 };
+            float[] solPruebaSinModa = { 1, 2, 3, 4 };
 
-        Estadistica es = new Estadistica();
+            Estadistica es = new Estadistica();
 
-        Assert.AreEqual(es.Moda(enteros), solEnteros);
-        Assert.AreEqual(es.Moda(decimales), solDecimales);
-        Assert.ThrowsException(es.Moda(caracteres));
-        Assert.AreEqual(es.Moda(pruebaModaMult), solPruebaModaMult);
-        Assert.AreEqual(es.Moda(pruebaSinModa), solPruebaSinModa);
-    }
+            int count = 0;
 
-    [TestMethod]
-    public void DesviacionAbsolutaTest()
-    {
-        int[] enteros = { 5, 4, 3, 2, 1};
-        float[] solEnteros = { 2, 1, 0, 1, 2};
-        float[] decimales = { 1.5f, 2.3f, 3.2f, 4.5f, 5.1f };
-        float[] solDecimales = { 1.7f, 0.9f, 0, 0.7f, 1.9f };
-        char[] caracteres = { 'a', 'b', 'c', 'd', 'e' };
-        int[] prueba= { 50, 22, 53, 4, 15};
-        float[] solPrueba = { 28, 0, 31, 18, 7 };
+            Assert.AreEqual(es.Moda(enteros).Length, solEnteros.Length);
 
-        Estadistica es = new Estadistica();
+            foreach (float num in es.Moda(enteros)) 
+            {
+                Assert.AreEqual(num, solEnteros[count]);
+                count++;
+            }
 
-        Assert.AreEqual(es.DesviacionAbsoluta(enteros), solEnteros);
-        Assert.AreEqual(es.DesviacionAbsoluta(decimales), solDecimales);
-        Assert.ThrowsException(es.DesviacionAbsoluta(caracteres));
-        Assert.AreEqual(es.DesviacionAbsoluta(prueba), solPrueba);
-    }
+            count = 0;
+            Assert.AreEqual(es.Moda(decimales).Length, solDecimales.Length);
+            
+            foreach (float num in es.Moda(solDecimales))
+            {
+                Assert.AreEqual(num, solDecimales[count]);
+                count++;
+            }
+            count = 0;
+            Assert.AreEqual(es.Moda(pruebaModaMult).Length, solPruebaModaMult.Length);
 
-    [TestMethod]
-    public void DesviacionMediaTest()
-    {
-        int[] enteros = { 5, 4, 3, 2, 1 };
-        float solEnteros = 1.2f;
-        float[] decimales = { 1.5f, 2.3f, 3.2f, 4.5f, 5.1f };
-        float solDecimales = 1.04f;
-        char[] caracteres = { 'a', 'b', 'c', 'd', 'e' };
-        int[] prueba = { 50, 22, 53, 4, 15 };
-        float solPrueba = 16.8f;
+            foreach (float num in es.Moda(pruebaModaMult))
+            {
+                Assert.AreEqual(num, solPruebaModaMult[count]);
+                count++;
+            }
+            count = 0;
+            Assert.AreEqual(es.Moda(pruebaSinModa).Length, solPruebaSinModa.Length);
 
-        Estadistica es = new Estadistica();
+            foreach (float num in es.Moda(pruebaSinModa))
+            {
+                Assert.AreEqual(num, solPruebaSinModa[count]);
+                count++;
+            }
+        }
 
-        Assert.AreEqual(es.DesviacionMedia(enteros), solEnteros);
-        Assert.AreEqual(es.DesviacionMedia(decimales), solDecimales);
-        Assert.ThrowsException(es.DesviacionMedia(caracteres));
-        Assert.AreEqual(es.DesviacionMedia(prueba), solPrueba);
+        [TestMethod]
+        public void DesviacionAbsolutaTest()
+        {
+            float[] enteros = { 5, 4, 3, 2, 1 };
+            float[] solEnteros = { 2, 1, 0, 1, 2 };
+            float[] decimales = { 1.5f, 2.3f, 3.2f, 4.5f, 5.1f };
+            float[] solDecimales = { 1.7f, 0.9f, 0, 1.3f, 1.9f };
+            float[] prueba = { 50, 22, 53, 4, 15 };
+            float[] solPrueba = { 28, 0, 31, 18, 7 };
+
+            Estadistica es = new Estadistica();
+
+            int count = 0;
+
+            Assert.AreEqual(es.DesviacionAbsoluta(enteros).Length, solEnteros.Length);
+
+            foreach (float num in es.DesviacionAbsoluta(enteros))
+            {
+                Assert.AreEqual(num, solEnteros[count]);
+                count++;
+            }
+
+            count = 0;
+
+            Assert.AreEqual(es.DesviacionAbsoluta(decimales).Length, solDecimales.Length);
+
+            foreach (float num in es.DesviacionAbsoluta(decimales))
+            {
+                Assert.AreEqual(num, solDecimales[count]);
+                count++;
+            }
+
+            count = 0;
+            
+            Assert.AreEqual(es.DesviacionAbsoluta(prueba).Length, solPrueba.Length);
+
+            foreach (float num in es.DesviacionAbsoluta(prueba))
+            {
+                Assert.AreEqual(num, solPrueba[count]);
+                count++;
+            }
+            //CollectionAssert.AreEqual(es.DesviacionAbsoluta(enteros), solEnteros);
+            //CollectionAssert.AreEqual(es.DesviacionAbsoluta(decimales), solDecimales);
+            //CollectionAssert.AreEqual(es.DesviacionAbsoluta(prueba), solPrueba);
+        }
+
+        [TestMethod]
+        public void DesviacionMediaTest()
+        {
+            float[] enteros = { 5, 4, 3, 2, 1 };
+            float solEnteros = 1.2f;
+            float[] decimales = { 1.5f, 2.3f, 3.2f, 4.5f, 5.1f };
+            float solDecimales = 1.16f;
+            float[] prueba = { 50, 22, 53, 4, 15 };
+            float solPrueba = 16.8f;
+
+            Estadistica es = new Estadistica();
+
+            Assert.AreEqual(es.DesviacionMedia(enteros), solEnteros);
+            Assert.AreEqual(es.DesviacionMedia(decimales), solDecimales);
+            Assert.AreEqual(es.DesviacionMedia(prueba), solPrueba);
+        }
     }
 }
