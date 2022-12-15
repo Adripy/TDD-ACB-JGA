5b9f9f001da38edd7046afb06078a196a5b6ffaf
Adripy <36918572+Adripy@users.noreply.github.com>
2022-12-15 18:54:47 +0100

    Sprint 2 - Se completa CodigoPostal

--- Clases/Validacion.cs
+++ Clases/Validacion.cs
@@ -11,18 +11,29 @@ namespace Clases
     {
         public string CodigoPostal(int codigoPostal)
         {
-            string ubicacion = Utils.GetAbsolutePath("..\\Datos\\Usuarios.csv");
-            System.IO.StreamReader archivo = new System.IO.StreamReader(ubicacion);
+            string ubicacion = Utils.GetAbsolutePath("..\\datos\\CPProvincia.csv");
+
+            System.IO.StreamReader archivo = new System.IO.StreamReader(ubicacion,System.Text.Encoding.UTF7);
             char separador = ';';
             string linea;
+
+            Dictionary<string, string> provincias = new Dictionary<string, string>();
 
             while ((linea = archivo.ReadLine()) != null)
             {
                 string[] fila = linea.Split(separador);
-                //Usuario u = new Usuario(fila[0], fila[1], fila[2], fila[3]);
-                //InsertarUsuario(u);
+                provincias.Add(fila[0], fila[1]); // se agregan las filas por pares con las columnas
             }
-            return null;
+
+            if (codigoPostal.ToString().Length != 5 )
+                return null;
+
+            if (codigoPostal > 52999)
+                return null;
+
+            string digitos = codigoPostal.ToString().Substring(0, 2);
+
+            return provincias[digitos];
         }
         public int NIF(string dni)
         {
