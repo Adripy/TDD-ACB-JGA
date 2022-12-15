49b56582c5b089b094ad9a3c92a06400ad7a7ff7
Adripy <36918572+Adripy@users.noreply.github.com>
2022-12-15 10:41:46 +0100

    Sprint 2 - Se inicia la Fase Roja, se crea ValidacionesTest

---
+++ ClasesTest/ValidacionTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ClasesTest
+{
+    [TestClass]
+    public class ValidacionTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
