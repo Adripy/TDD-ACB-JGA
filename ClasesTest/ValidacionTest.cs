20c2c478eff528e43aa4275b2344c3d529910176
Adripy <36918572+Adripy@users.noreply.github.com>
2022-12-15 12:32:29 +0100

    Sprint 2 - Se crean el resto de tests salvo Email

--- ClasesTest/ValidacionTest.cs
+++ ClasesTest/ValidacionTest.cs
@@ -72,5 +72,71 @@ namespace ClasesTest
                 Assert.AreEqual(pares.Key, NIF(pares.Value));
             }
         }
+
+        [TestMethod]
+        public void TarjetaCreditoTest()
+        {
+            double tcLargo = 5186484156541656456;
+            double tcCorto = 26;
+            double cpMS = 5540708689080426; // mastercard
+            double cpVisa16 = 4483627710043819; // visa 16
+            double cpVisa13 = 4038815174749; // visa 13
+
+            Validacion val = new Validacion();
+
+            Assert.AreEqual(0, TarjetaCredito(tcLargo));
+            Assert.AreEqual(0, TarjetaCredito(tcCorto));
+            Assert.AreEqual(1, TarjetaCredito(cpMS));// mastercard
+            Assert.AreEqual(1, TarjetaCredito(cpVisa16)); //visa 16 digitos
+            Assert.AreEqual(1, TarjetaCredito(cpVisa13)); //visa 13 digitos
+        }
+
+        [TestMethod]
+        public void CuentaCorrienteClienteTest()
+        {
+            string cccLargo = "5186484156541656456685416485";
+            string cccCorto = "26";
+            string cccConLetras = "12345678x12345678920";
+            string cccConEspecial = "12345678*12345678920";
+            string cccMal = "12345678912345678920";
+            string ccc = "21039777361834071062"; 
+            string ccc1 = "01259353619227879972";
+
+            Validacion val = new Validacion();
+
+            Assert.AreEqual(0, CuentaCorrienteCliente(cccLargo));
+            Assert.AreEqual(0, CuentaCorrienteCliente(cccCorto));
+            Assert.AreEqual(0, CuentaCorrienteCliente(cccConLetras));
+            Assert.AreEqual(0, CuentaCorrienteCliente(cccConEspecial));
+            Assert.AreEqual(0, CuentaCorrienteCliente(cccMal));
+            Assert.AreEqual(1, CuentaCorrienteCliente(ccc));
+            Assert.AreEqual(1, CuentaCorrienteCliente(ccc1));
+        }
+
+        [TestMethod]
+        public void IBANClienteTest()
+        {
+            string ibanLargo = "518648415654165645668541648565416451251";
+            string ibanCorto = "26";
+            string ibanSinES= "12345678123456789201234";
+            string ibanConLetrasMid = "ES3456781234x6789201234";
+            string ibanConEspecial = "ES345678*1234/6789201234";
+            string ibanMal = "123456789123456789201234";
+            string iban = "ES8120804769486758368126";
+            string iban1 = "ES0201828300476366645481";
+
+            Validacion val = new Validacion();
+
+            Assert.AreEqual(0, IBAN(ibanLargo));
+            Assert.AreEqual(0, IBAN(ibanCorto));
+            Assert.AreEqual(0, IBAN(ibanSinES));
+            Assert.AreEqual(0, IBAN(ibanConLetrasMid));
+            Assert.AreEqual(0, IBAN(ibanConEspecial));
+            Assert.AreEqual(0, IBAN(ibanMal));
+            Assert.AreEqual(1, IBAN(iban));
+            Assert.AreEqual(1, IBAN(iban1));
+        }
+
+
     }
 }
