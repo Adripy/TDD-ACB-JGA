497edc3fe5828ee4a249f11ac359b6672d24697b
Adripy <36918572+Adripy@users.noreply.github.com>
2022-12-15 18:09:21 +0100

    Solucionado problema de referencias(Ya compila)

--- Clases/Estadistica.cs
+++ Clases/Estadistica.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-namespace ClasesTest
+namespace Clases
 {
     public class Estadistica
     {
--- Clases/Validacion.cs
+++ Clases/Validacion.cs
@@ -1,17 +1,17 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Comun;
 
-namespace ClasesTest
+namespace Clases
 {
     public class Validacion
     {
         public string CodigoPostal()
         {
-            string ubicacion = GetAbsolutePath("..\\Datos\\Usuarios.csv");
+            string ubicacion = Utils.GetAbsolutePath("..\\Datos\\Usuarios.csv");
             System.IO.StreamReader archivo = new System.IO.StreamReader(ubicacion);
             char separador = ';';
             string linea;
@@ -19,8 +19,8 @@ namespace ClasesTest
             while ((linea = archivo.ReadLine()) != null)
             {
                 string[] fila = linea.Split(separador);
-                Usuario u = new Usuario(fila[0], fila[1], fila[2], fila[3]);
-                InsertarUsuario(u);
+                //Usuario u = new Usuario(fila[0], fila[1], fila[2], fila[3]);
+                //InsertarUsuario(u);
             }
             return null;
         }
